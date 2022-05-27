b3c47db42f84e5fa71e982bf4ec9530225221452
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-05-27 15:45:19 +0200

    Implementación de la API REST - Configurar la conexión a la base de datos

--- REST_SD/appSD.js
+++ REST_SD/appSD.js
@@ -1,15 +1,35 @@
+// Dependencias necesarias
 const express = require("express");
-
-const app = express();
+const mysql = require ("mysql");
+const bodyParser = require("body-parser");
 
 // Se define el puerto
 const port = 3000;
 
-app.get("/", (req, res) => {
+// Se crea instancia de express
+const appSD = express();
+
+// Configuración de la conexión a la base de datos MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // Tenemos que usar el usuario que hayamos creado en mysql workbench
+    password: '1234', // Tenemos que poner la contraseña que tengamos en nuestro usuario local
+    database: 'SD_MYSQL'
+});
+
+// Comprobar conexión a la base de datos
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Conexión a la base de datos SD_MYSQL correcta');
+});
+
+// Rutas de la aplicación
+// Página por defecto
+appSD.get("/", (req, res) => {
     res.json({message:'Página de inicio de aplicación de ejemplo de SD'})
 });
 
-// Ejecutar la aplicación
-app.listen(port, () => {
+// Ejecutar la aplicación / Arrancar el servidor
+appSD.listen(port, () => {
     console.log(`Ejecutando la aplicación API REST de SD en el puerto ${port}`);
 });
