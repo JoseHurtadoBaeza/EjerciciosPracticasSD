5092f814ce5d3b6e3d3ae056a36a80e151a52a89
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-05-30 03:21:19 +0200

    Terminado - Generación de un servidor API REST sobre https mediante node.js

--- REST_SD_SECURE/serverAPIREST.js
+++ REST_SD_SECURE/serverAPIREST.js
@@ -34,17 +34,48 @@ mrouter.map(function () {
     /* GET request con 3 parámetros codificados mediante expresiones regulares.
     ** https://localhost:8080/param1/id1/param2/hola/param3/mundo 
     */
-    this.get(/^param1\/([A-Za-z0-9_]+)\/param2+\/([A-Za-z0-9_]+)\/param3+\/([A-Za-z0-9_]+)$/).bind(function (req, res, param1, param2, param3, param3) {
-        res.send("GET p1 " + param1 + " p2: " + param2 + " p3: " + param3);
+    this.get(/^param1\/([A-Za-z0-9_]+)\/param2+\/([A-Za-z0-9_]+)\/param3+\/([A-Za-z0-9_]+)$/).bind(function (req, res, param1, param2, param3) {
+        res.send('GET p1 '+param1+ ' p2: '+param2 + ' p3: '+ param3);
     });
+    
 
     /* PUT request con varios parámetros codificados mediante expresiones regulares.
     ** https://localhost:8080/param1/id1/param2/hola/param3/mundo
     */
-    this.put(/^param\/([A-Za-z0-9_]+)\/param2+\/([A-Za-z0-9_]+)\/param3+\/([A-Za-z0-9_]+)$/).bind(function (req, res, param1, param2, param3, data) {
-        res.send("PUT " + param1 + " p1: " + param2 + " p2: " + param3);
+    this.put(/^param1\/([A-Za-z0-9_]+)\/param2+\/([A-Za-z0-9_]+)\/param3+\/([A-Za-z0-9_]+)$/).bind(function (req, res, param1, param2, param3,data) {
+        res.send('PUT '+param1+ ' p1: '+param2 + ' p2: '+ param3);
     });
-
     
 
+    /** POST request con varios parámetros codificados mediante expresiones regulares.
+     *  https://localhost:8080/param1/id1/param2/hola/param3/mundo
+     */
+    this.post(/^param1\/([A-Za-z-0-9_]+)\/param2+\/([A-Za-z0-9_]+)\/param3+\/([A-Za-z0-9_]+)$/).bind(function (req, res, param1, param2, param3, data) {
+        res.send("POST " + param1 + " p1: " + param2 + " p2: " + param3);
+    });
+
+    /** DELETE request con varios parámetros codificados mediante expresiones regulares. *
+     *  https://localhost:8080/delete/usuario/id/25
+     */
+    this.get(/^delete\/([A-Za-z0-9_]+)\/id+\/([A-Za-z0-9_]+)$/).bind(function (req, res, entidad, id) {
+        res.send("DELETE " + entidad + " id: " + id);
+    });
+
 }); // end mapping
+
+// Creación del servidor https
+https.createServer(options, function (request, response) {
+    var body = "";
+    
+    request.addListener("data", function (chunk) { body += chunk });
+    request.addListener("end", function () {
+
+        mrouter.handle(request, body, function (result) {
+
+            response.writeHead(result.status, result.headers);
+            response.end(result.body);
+
+        });
+
+    });
+}).listen(8080);
